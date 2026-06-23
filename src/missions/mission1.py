0dc5876135cd69da2d53f123d321268011f2a247
Rishi <rishi.prabhakar@outlook.com>
2026-06-22 17:48:50 -0700

    Revert "Work on krill collection"
    This reverts commit f8ea55f9046524ad3c27ccc7218563502461b211.c
    
    (I left my code on another branch. I will move it to another file and merge it later)
    
    Signed-off-by: Rishi <rishi.prabhakar@outlook.com>

--- src/missions/mission1.py
+++ src/missions/mission1.py
@@ -14,27 +14,6 @@ from robot.arms import(
     move_right_arm_to
 )
 
-from robot.Sounds import(
-    play_handoff_to_mission_code_ding_dong,
-    play_handoff_to_nav_code_ding_dong
-)
-
-STRAIGHT_AMOUNT = 1000
-TURN_AMOUNT = 100
-
 def run():
-    for i in range(100):
-        print(f"Run {i} start")
-        play_handoff_to_mission_code_ding_dong()
-        drive_backward(STRAIGHT_AMOUNT)
-        play_handoff_to_nav_code_ding_dong()
-        turn_left(TURN_AMOUNT)
-        play_handoff_to_mission_code_ding_dong()
-        drive_backward(STRAIGHT_AMOUNT)
-        Sounds.play_handoff_to_nav_code_ding_dong()
-        turn_right(TURN_AMOUNT)
-        print(f"Run {i} done")
-    
-if __name__ == "__main__":
-    import main
-    main.main()
+    drive_forward(100)
+    drive_backward(100)
